e1d3b4fd4570bdda71d7f252b39e36947fbdd128
Bartlomiej Nawoj <thebalowen@gmail.com>
2019-01-01 17:09:24 +0100

    added buttons and movement to game class

--- PacMan/Game.cpp
+++ PacMan/Game.cpp
@@ -1,5 +1,5 @@
 #include "Game.h"
-
+#include <iostream>
 
 
 Game::Game()
@@ -22,6 +22,23 @@ void Game::run()
 		{
 			if (event.type == sf::Event::Closed)	//if user clicks exit button
 				m_window.close();
+
+			// if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))  it is better for const pressing button
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::S)
+					pressStart();
+				
+				if (event.key.code == sf::Keyboard::Up)
+					movePacman(sf::Vector2i(0, -1));
+				if (event.key.code == sf::Keyboard::Down)
+					movePacman(sf::Vector2i(0, 1));
+				if (event.key.code == sf::Keyboard::Left)
+					movePacman(sf::Vector2i(-1, 0));
+				if (event.key.code == sf::Keyboard::Right)
+					movePacman(sf::Vector2i(1, 0));
+
+			}
 		}
 
 		m_window.clear();
@@ -29,3 +46,14 @@ void Game::run()
 		m_window.display();
 	}
 }
+
+void Game::pressStart()
+{
+	std::cout << "Kliknales start" << std::endl;
+}
+
+void Game::movePacman(sf::Vector2i direction)
+{
+	std::cout << "Pacman sie rusza" << std::endl;
+}
+
