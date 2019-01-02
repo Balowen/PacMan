7068f28a8993a171874d593bd0404d9bad974840
Bartlomiej Nawoj <thebalowen@gmail.com>
2019-01-03 00:13:55 +0100

    Created dots for pacman to eat

---
+++ PacMan/Capsules.cpp
@@ -0,0 +1,11 @@
+#include "Capsules.h"
+
+sf::CircleShape dot()
+{
+	return sf::CircleShape();
+}
+
+sf::CircleShape bigDot()
+{
+	return sf::CircleShape();
+}
---
+++ PacMan/Capsules.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+//sfml already has a class circleShape
+//and capsules are circleShapes (dots) which Pacman eats
+
+sf::CircleShape dot();
+sf::CircleShape bigDot();
