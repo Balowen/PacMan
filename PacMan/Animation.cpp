6a0858b69f7f79067ffdb2ad9a161676ed370f2c
Bartlomiej Nawoj <thebalowen@gmail.com>
2019-01-03 22:41:00 +0100

    Animation sie kompiluje xD

--- PacMan/Animation.cpp
+++ PacMan/Animation.cpp
@@ -3,10 +3,10 @@
 
 
 Animation::Animation()
-:currentFrame(0)
+:current_frameIndex(0)
 ,m_animationTime(sf::Time::Zero)
 ,m_isPlaying(false)
-,m_keepAnimating(false)
+,m_loopAnimation(false)
 {
 }
 
@@ -36,6 +36,25 @@ void Animation::update(sf::Time elapsedTime)
 {
 	if (!isPlaying())				//if we didn't call animate, don't do anything
 		return;
+
+	static sf::Time buffer = sf::Time::Zero;
+	buffer += elapsedTime;		//it will always be = elapsedTime
+
+	// single frame animation time = wholeAnimationTime / number of frames in vector m_frames
+	sf::Time single_frameTime = m_animationTime / static_cast<float>(m_frames.size());
+												//because size() returns uint
+	while (buffer > single_frameTime)	//if we have elapsedTime to animate
+	{
+		current_frameIndex++;
+		if (current_frameIndex == m_frames.size())	
+		{
+			if (!m_loopAnimation)
+				m_isPlaying = false;	//dont want to loopAnimation, then stop playing
+
+			current_frameIndex = 0;	//else, keep changing animations 
+		}
+		buffer -= single_frameTime;
+	}
 }
 
 void Animation::animate(sf::Sprite & sprite)
