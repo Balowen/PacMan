0f6e9325b7a62923887ad753d86923ced513e6d1
Bartlomiej Nawoj <thebalowen@gmail.com>
2019-01-03 23:55:11 +0100

    pacman animation added (from spritesheet)

--- PacMan/PacMan.cpp
+++ PacMan/PacMan.cpp
@@ -7,7 +7,25 @@ PacMan::PacMan(sf::Texture& texture)
 ,m_isDying(false)
 ,m_isDead(false)
 {
-	m_sprite.setOrigin(7, 7);		// Pacman sprite is 14,14
+	m_sprite.setOrigin(7, 7);		// Pacman sprite is 14 x 14
+
+	m_aliveAnimation.addFrame(sf::IntRect(1, 1, 14, 14));
+	m_aliveAnimation.addFrame(sf::IntRect(1 + 18, 1, 14, 14));
+
+	m_dieAnimation.addFrame(sf::IntRect(36, 1, 14, 14));
+	m_dieAnimation.addFrame(sf::IntRect(36+16, 1, 14, 14));
+	m_dieAnimation.addFrame(sf::IntRect(36 + 31, 1, 15, 15));
+	m_dieAnimation.addFrame(sf::IntRect(36 + 31, 1, 15, 15));
+	m_dieAnimation.addFrame(sf::IntRect(36 + 47, 1, 15, 15));
+	m_dieAnimation.addFrame(sf::IntRect(36 + 63, 1, 15, 15));
+	m_dieAnimation.addFrame(sf::IntRect(36 + 79, 1, 15, 15));
+	m_dieAnimation.addFrame(sf::IntRect(36 + 95, 1, 15, 15));
+	m_dieAnimation.addFrame(sf::IntRect(36 + 111, 1, 15, 15));
+	m_dieAnimation.addFrame(sf::IntRect(36 + 127, 1, 15, 15));
+	m_dieAnimation.addFrame(sf::IntRect(36 + 143, 1, 15, 15));
+	m_dieAnimation.addFrame(sf::IntRect(36 + 159, 1, 15, 15));
+
+	m_aliveAnimation.play(sf::milliseconds(25), true);	//0.25 per frame and loop animation
 
 }
 
@@ -28,7 +46,11 @@ void PacMan::draw(sf::RenderTarget& target, sf::RenderStates states) const
 
 void PacMan::die()
 {
-	m_isDead = true;
+	if (!m_isDying)
+	{
+		m_dieAnimation.play(sf::seconds(1),false);
+		m_isDying = true;
+	}
 }
 
 bool PacMan::isDead() const
